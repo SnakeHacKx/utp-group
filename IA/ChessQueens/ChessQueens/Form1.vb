f85944618440fb2b6693b5b3b750ebb1050d9683
DrackoX9 <joseph13rios@hotmail.com>
2021-05-07 18:40:17 -0600

    Problema en el metodo recursivo

--- IA/ChessQueens/ChessQueens/Form1.vb
+++ IA/ChessQueens/ChessQueens/Form1.vb
@@ -41,6 +41,7 @@
     ''' </summary>
     ''' <param name="fila">Fila actual.</param>
     Private Sub ColocarReina(fila As Integer)
+        'Dim temp As New  List(Of Integer)()
         For columna = 0 To numeroDeReinas
             If Not PosicionEsSegura(fila, columna) Then
                 Continue For
@@ -50,7 +51,15 @@
                 'Si es la ultima fila agregamos una copia de la lista de soluciones
                 'actuales a la lista de soluciones
                 If fila = numeroDeReinas - 1 Then
-                    soluciones.AddRange(solucionActual)
+                    'temp = solucionActual
+                    soluciones.Add(solucionActual)
+                    'solucionActual = New List(Of Integer)
+                    'solucionActual = temp
+                    Dim a As String = ""
+                    For x = 0 To numeroDeReinas - 1
+                        a &= " " & soluciones(soluciones.Count - 1).Item(x).ToString
+                    Next
+                    ListBox1.Items.Add(a)
                 Else
                     'Si no estamos en la ultima fila, seguimos la recursividad.
                     ColocarReina(fila + 1)
@@ -62,7 +71,7 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         numeroDeReinas = Convert.ToInt32(TextBox1.Text)
 
-        For x = 0 To numeroDeReinas
+        For x = 0 To numeroDeReinas - 1
             solucionActual.Add(0)
         Next
 
@@ -70,8 +79,13 @@
         ColocarReina(0)
         ListBox1.Items.Add("Resultados para " + Str(numeroDeReinas) + " Reinas:")
         ListBox1.Items.Add(Str(soluciones.Count) + " soluciones encontradas")
-        For Each solucion In soluciones
-            ListBox1.Items.Add(solucion)
-        Next
+        'For i = 0 To soluciones.Count - 1
+        '    Dim a As String = ""
+        '    For x = 0 To numeroDeReinas - 1
+        '        a &= " " & soluciones(i).Item(x).ToString
+        '    Next
+        '    ListBox1.Items.Add(a)
+
+        'Next
     End Sub
 End Class
